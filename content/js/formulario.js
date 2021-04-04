f56ae1f464a13cd64c28c411cd8cedbf5dd6e155
Gabriel Jordan <gabrijordan3@gmail.com>
2021-04-04 20:02:02 -0300

    lgin adicionado em todas as paginas desktop

--- content/js/formulario.js
+++ content/js/formulario.js
@@ -11,8 +11,13 @@ document.getElementById("btn-close").addEventListener("click", function(){
 });
 //gambiarra q n sei usar 
 
+var btn = document.querySelector("#js-btn");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
 
 //Scipt mobile
+
 document.getElementById("btn-mbl").addEventListener("click", function(){
     document.querySelector(".form-container").style.display = "flex";
     document.querySelector("body").style.overflow = "hidden";
