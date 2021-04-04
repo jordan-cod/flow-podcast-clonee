901ecd28dba0ddf6f1f119df6bf8df62f3bf9cba
Gabriel Jordan <gabrijordan3@gmail.com>
2021-04-04 19:30:01 -0300

    mobile login adicionado

--- content/js/formulario.js
+++ content/js/formulario.js
@@ -7,7 +7,13 @@ document.getElementById("js-btn").addEventListener("click", function(){
 document.getElementById("btn-close").addEventListener("click", function(){
     document.querySelector(".form-container").style.display = "none";
     document.querySelector("body").style.overflow = "auto";
-    window.scrollTo(0-coord, 0-coord);
+    
 });
 //gambiarra q n sei usar 
 
+
+//Scipt mobile
+document.getElementById("btn-mbl").addEventListener("click", function(){
+    document.querySelector(".form-container").style.display = "flex";
+    document.querySelector("body").style.overflow = "hidden";
+});
