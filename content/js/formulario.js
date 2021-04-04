98c2c31f0d044c5bc80be3ddf7428c488c83f274
Gabriel Jordan <gabrijordan3@gmail.com>
2021-04-04 16:34:38 -0300

    gambiarra no menu pra abrir e fechar

--- content/js/formulario.js
+++ content/js/formulario.js
@@ -1,5 +1,10 @@
 document.getElementById("js-btn").addEventListener("click", function(){
     document.querySelector(".form").style.display = "grid";
+});
+
+
+document.getElementById("btn-close").addEventListener("click", function(){
+    document.querySelector(".form").style.display = "none";
 })
 
 
